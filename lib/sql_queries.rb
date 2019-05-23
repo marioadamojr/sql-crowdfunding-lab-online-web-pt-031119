2972b6b232cf0674f5ce209032c7485f8228846d
Mario Adamo Jr <marioadamojr@gmail.com>
2019-05-23 01:00:01 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -29,7 +29,8 @@ def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_
   FROM projects
   JOIN pledges
   ON projects.id = pledges.project_id
-  GROUP BY pledges.project_id  "
+  GROUP BY pledges.project_id  
+  HAVING projects.funding_goal <= SUM(pledges.amount)"
 
 end
 
